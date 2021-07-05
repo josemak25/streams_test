7f800da53b6edc950e0c9c300318f70a30a61933
“josemak25” <amakirij@gmail.com>
2021-07-05 21:34:16 +0100

    add scale method as theme provider utils functions

---
+++ src/utils/scale.ts
@@ -0,0 +1,16 @@
+import {PixelRatio} from 'react-native';
+import isTablet from './isTablet';
+
+/**
+ *
+ * @description scale a method for scaling app fonts based off the pixel density of the users phone
+ * @function scale
+ * @property size {number}
+ * @returns number
+ */
+const scale = (size: number) => {
+  const density = isTablet() ? 1.2 : 1;
+  return Math.round(PixelRatio.roundToNearestPixel(size * density));
+};
+
+export default scale;
