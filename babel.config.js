ae3d91f1d7184cae7fb705bd6fbb67361b474af1
“josemak25” <amakirij@gmail.com>
2021-07-05 21:10:25 +0100

    remove react native boiler code and add module mapper

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': ['./src'],
+            '@assets': ['./assets'],
+            '@typings': ['./typings'],
+            '@utils': ['./src/utils'],
+            '@config': ['./src/config'],
+            '@screens': ['./src/screens'],
+            '@constants': ['./src/constants'],
+            '@providers': ['./src/providers'],
+            '@navigation': ['./src/navigation'],
+            '@components': ['./src/components'],
+          },
+        },
+      ],
+    ],
+  };
 };
